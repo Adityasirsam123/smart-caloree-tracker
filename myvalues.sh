fc572d8b78c93b0f8c2e69e7cc295f0cd9871f23
Aditya  Sirsam <156391626+Adityasirsam123@users.noreply.github.com>
2025-07-31 16:13:18 +0530

    Create myvalues.sh

---
+++ myvalues.sh
@@ -0,0 +1,11 @@
+USERNAME="Adityasirsam"
+PASSWORD="glpat--7-1Vzf5LukPvpUNxqAx3286MQp1OmduaGV2Cw.01.121mu2gsc"
+EMAIL="aditya@oyebusy.in"
+
+AUTH=$(echo -n "$USERNAME:$PASSWORD" | base64)
+DOCKER_JSON=$(jq -nc --arg u "$USERNAME" --arg p "$PASSWORD" --arg e "$EMAIL" --arg a "$AUTH" \
+  '{auths: {"https://registry.gitlab.com": {username: $u, password: $p, email: $e, auth: $a}}}')
+
+echo "$DOCKER_JSON" | base64 -w 0
+
+ubuntu@ip-172-31-34-211:~/smart-caloree-tracker$
